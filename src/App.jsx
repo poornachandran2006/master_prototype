14d0cd763a2cf4d8ec2e5803ad255a234c3dcadd
poornachandran2006 <poorna2006poorna@gmail.com>
2025-09-15 16:39:33 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import SlaveApplication from './components/SlaveApplication'
-// import MasterApplication from './components/MasterApplication'
+import MasterApplication from './components/MasterApplication'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <MasterApplication /> */}
-      <SlaveApplication />
+      <MasterApplication /> 
     </>
   )
 }
